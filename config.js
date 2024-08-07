143f6e3afb94a79b178c6d4963964a75d92dc524
Juan Camilo <juancamilor576@gmail.com>
2024-08-06 22:18:44 -0500

    fix: add data config

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 import "dotenv/config";
 
 export const config = {
-  PRODUCTION_DB: process.env.PRODUCTION_DB || "",
-  DEVELOPMENT_DB: process.env.DEVELOPMENT_DB || "",
-  DB_HOST: process.env.DB_HOST || "",
-  DB_USER: process.env.DB_USER || "",
-  DB_PASSWORD: process.env.DB_PASSWORD || "",
-  TOKEN_SECRET: process.env.TOKEN_SECRET || "",
-  ENVIRONMENT: process.env.ENVIRONMENT || "",
-  APP_PORT: process.env.APP_PORT || "",
-  APP_PORT_DB: process.env.APP_PORT_DB || "",
-  EMAIL: process.env.EMAIL || "",
-  PASSWORD_EMAIL: process.env.PASSWORD_EMAIL || "",
-  PASSWORD_APPLICATION: process.env.PASSWORD_APPLICATION || ""
+  PRODUCTION_DB: process.env.PRODUCTION_DB || "votaciones_db",
+  DEVELOPMENT_DB: process.env.DEVELOPMENT_DB || "votaciones_db",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_USER: process.env.DB_USER || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "i(7PoNKpbsmueSqh",
+  TOKEN_SECRET: process.env.TOKEN_SECRET || "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+  ENVIRONMENT: process.env.ENVIRONMENT || "production",
+  APP_PORT: process.env.APP_PORT || "1338",
+  APP_PORT_DB: process.env.APP_PORT_DB || "3306",
+  EMAIL: process.env.EMAIL || "sistemaintegraldeinformacionel@gmail.com",
+  PASSWORD_EMAIL: process.env.PASSWORD_EMAIL || "SistemaIntegralInformacionElectoral1",
+  PASSWORD_APPLICATION: process.env.PASSWORD_APPLICATION || "gpufdeigmhdssout"
 };
