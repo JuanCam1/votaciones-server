1cc1ff0fb5fcaecece18287bc184da85fc1c650e
Juan Camilo <juancamilor576@gmail.com>
2024-08-07 17:24:15 -0500

    change: config.js

--- config.js
+++ config.js
@@ -1,28 +1,32 @@
 import "dotenv/config";
 
+// export const config = {
+//   PRODUCTION_DB: process.env.PRODUCTION_DB || "",
+//   DEVELOPMENT_DB: process.env.DEVELOPMENT_DB || "",
+//   DB_HOST: process.env.DB_HOST || "",
+//   DB_USER: process.env.DB_USER || "",
+//   DB_PASSWORD: process.env.DB_PASSWORD || "",
+//   TOKEN_SECRET: process.env.TOKEN_SECRET || "",
+//   ENVIRONMENT: process.env.ENVIRONMENT || "",
+//   APP_PORT: process.env.APP_PORT || "",
+//   APP_PORT_DB: process.env.APP_PORT_DB || "",
+//   EMAIL: process.env.EMAIL || "",
+//   PASSWORD_EMAIL: process.env.PASSWORD_EMAIL || "",
+//   PASSWORD_APPLICATION: process.env.PASSWORD_APPLICATION || ""
+// };
+
 export const config = {
-  PRODUCTION_DB: process.env.PRODUCTION_DB || "",
-  DEVELOPMENT_DB: process.env.DEVELOPMENT_DB || "",
-  DB_HOST: process.env.DB_HOST || "",
-  DB_USER: process.env.DB_USER || "",
-  DB_PASSWORD: process.env.DB_PASSWORD || "",
-  TOKEN_SECRET: process.env.TOKEN_SECRET || "",
-  ENVIRONMENT: process.env.ENVIRONMENT || "",
-  APP_PORT: process.env.APP_PORT || "",
-  APP_PORT_DB: process.env.APP_PORT_DB || "",
-  EMAIL: process.env.EMAIL || "",
-  PASSWORD_EMAIL: process.env.PASSWORD_EMAIL || "",
-  PASSWORD_APPLICATION: process.env.PASSWORD_APPLICATION || ""
-};
-// PRODUCTION_DB: "votaciones_db",
-// DEVELOPMENT_DB: "votaciones_db",
-// DB_HOST: "localhost",
-// DB_USER: "root",
-// DB_PASSWORD: "i(7PoNKpbsmueSqh",
-// TOKEN_SECRET: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-// ENVIRONMENT: "production",
-// APP_PORT: "1338",
-// APP_PORT_DB: "3306",
-// EMAIL: "sistemaintegraldeinformacionel@gmail.com",
-// PASSWORD_EMAIL: "SistemaIntegralInformacionElectoral1",
-// PASSWORD_APPLICATION: "gpufdeigmhdssout"
+
+  PRODUCTION_DB: "votaciones_db",
+  DEVELOPMENT_DB: "votaciones_db",
+  DB_HOST: "localhost",
+  DB_USER: "root",
+  DB_PASSWORD: "i(7PoNKpbsmueSqh",
+TOKEN_SECRET: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+ENVIRONMENT: "production",
+APP_PORT: "1338",
+APP_PORT_DB: "3306",
+EMAIL: "sistemaintegraldeinformacionel@gmail.com",
+PASSWORD_EMAIL: "SistemaIntegralInformacionElectoral1",
+PASSWORD_APPLICATION: "gpufdeigmhdssout"
+}
